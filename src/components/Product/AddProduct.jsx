4d086dfad90c7dd7c5329f25e1af5781196162f2
Chameng Vang <Chameng.vang@outlook.com>
2022-10-16 15:24:45 -0500

    update add product component with material ui for visual.

--- src/components/Product/AddProduct.jsx
+++ src/components/Product/AddProduct.jsx
@@ -7,99 +7,103 @@ import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Container, Stack } from '@mui/system';
+import { Card, Typography } from '@mui/material';
 
 function AddProduct () {
 
-        // USE-STATE
-        const [previewSource, setPreviewSource] = useState ('');
-        const [brandName, setBrandName] = useState('');
-        const [description, setDescription] = useState('');
+    // USE-STATE
+    const [previewSource, setPreviewSource] = useState ('');
+    const [brandName, setBrandName] = useState('');
+    const [description, setDescription] = useState('');
 
-        // DISPATCH 
-        const dispatch = useDispatch();
-        const history = useHistory();
-    
-        // HANDLE THE FILE CHANGE INPUT 
-        const handleFileInputChange = e => {
-            const file = e.target.files[0];
-            previewFile(file);
-        }
-    
-        // THIS IS USED TO PREVIEW THE IMAGE FILE SLECTED ON TO THE DOM.
-        const previewFile = (file) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onloadend = () => {
-                setPreviewSource(reader.result);
-            }
-        }
-    
-        // HANDLE THE SUBMIT BUTTON 
-        const handelSubmitFile = e => {
-            e.preventDefault();
-            if(!previewSource || !brandName) return;
-            uploadPost(previewSource);
-            history.push('/product');
-            setPreviewSource('');
-            setBrandName('');
-            setDescription('');
-        }
+    // DISPATCH 
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-        // HANDLE THE CANCEL BUTTON
-        const handleCancel = (e) => {
-            e.preventDefault();
-            history.push('/product')
+    // HANDLE THE FILE CHANGE INPUT 
+    const handleFileInputChange = e => {
+        const file = e.target.files[0];
+        previewFile(file);
+    }
+
+    // THIS IS USED TO PREVIEW THE IMAGE FILE SLECTED ON TO THE DOM.
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
         }
-    
-    
-        // THIS IS OUR IMAGE FILE DATA, WE ARE CONVERTING THE IMAGE 
-        // FILE INTO A STRING TO SEND IT AS OUT DATA TO SAGA. 
-        const  uploadPost = (base64EncodedImage) => {
-            dispatch({
-                type: 'SAGA_ADD_PRODUCT',
-                payload:{
-                    product_url:base64EncodedImage,
-                    brand_name: brandName,
-                    description: description
-                },
-                headers:{'Content-type': 'application/json'}
-            })
-        } 
+    }
+
+    // HANDLE THE SUBMIT BUTTON 
+    const handelSubmitFile = e => {
+        e.preventDefault();
+        if(!previewSource || !brandName) return;
+        uploadPost(previewSource);
+        // THIS HARD REFRESH PAGE. WILL NEED TO FIX THIS FOR
+        // FUTURE USE. NEED TO ADD ALERT TO NOTIFY USER THAT THIER 
+        // POST IS SUCCESSFUL. 
+        window.location.reload();
+    }
+
+    // HANDLE THE CANCEL BUTTON
+    const handleCancel = (e) => {
+        e.preventDefault();
+        history.push('/product')
+    }
+
+
+    // THIS IS OUR IMAGE FILE DATA, WE ARE CONVERTING THE IMAGE 
+    // FILE INTO A STRING TO SEND IT AS OUT DATA TO SAGA. 
+    const  uploadPost = (base64EncodedImage) => {
+        dispatch({
+            type: 'SAGA_ADD_PRODUCT',
+            payload:{
+                product_url:base64EncodedImage,
+                brand_name: brandName,
+                description: description
+            },
+            headers:{'Content-type': 'application/json'}
+        })
+    } 
 
     return (
-        <div>
-            <div>
+        <Box
+        component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
+        noValidate
+        autoComplete="off"
+        >
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
+            >                    
+            <Typography variant='h3' display='wrap'>Add Your Skincare Product!</Typography>
+            <Card sx={{ maxWidth: 345 }}>
+                <Container direction="center">
+                {previewSource && (<img src={previewSource} alt="image" />)}
                 <input type="file" onChange={handleFileInputChange} name="image" />
-                <Box 
-                    component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <div>
-                        <TextField 
-                            id="outlined-basic" 
-                            label="Brand Name" 
-                            variant="outlined"
-                            onChange={(e) => setBrandName(e.target.value)}
-                         />
-                    </div>
-                    <div>
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Description"
-                            multiline
-                            rows={4}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </div>
-                </Box>
-                <Button variant="contained" onClick={handelSubmitFile}>Add</Button>
-                <Button variant="contained" onClick={handleCancel}>Cancel</Button>
-            </div>
-            {/* {previewSource && (
-                <img src={previewSource} alt="chosen" />
-            )} */}
-        </div>
+                <TextField 
+                    id="outlined-basic" 
+                    label="Brand Name" 
+                    variant="outlined"
+                    onChange={(e) => setBrandName(e.target.value)}
+                />
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Description"
+                    multiline
+                    rows={4}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                </Container>
+            </Card>
+            <Button variant="contained" onClick={handelSubmitFile}>Add</Button>
+            <Button variant="contained" onClick={handleCancel}>Back</Button>
+            </Stack>
+        </Box>
     )
 }
 
