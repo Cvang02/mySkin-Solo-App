f162c5de7171758afbfa40fca230c0fc3cc57f5d
Chameng Vang <Chameng.vang@outlook.com>
2022-10-18 11:36:37 -0500

    Update landing page and registration page with material ui for visual.

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,10 +2,24 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // IMPORT MATERIAL UI
-import { Button, Hidden, TextField, Typography } from '@mui/material';
-import { Stack } from '@mui/system';
+import { Avatar, Box, Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" to="/home">
+        Chameng Vang
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 function RegisterForm() {
 
@@ -24,6 +38,7 @@ function RegisterForm() {
   // REGISTER HANDLE SUBMIT
   const registerUser = (event) => {
     event.preventDefault();
+    if (!username || !password || !email) return;
     dispatch({
       type: 'REDUCER_USER',
       payload: {
@@ -36,58 +51,84 @@ function RegisterForm() {
   }; // END OF registerUser
 
   return (
-    <>
-<form onSubmit={registerUser}>
-  <Stack             
-    direction="column"
-    justifyContent="center"
-    alignItems="center"
-    spacing={1}
-  >
-    <Typography>Register User</Typography>
-    {errors.registrationMessage && (
-        <h3 className="alert" role="alert">
-          {errors.registrationMessage}
-        </h3>
-      )}
-    <TextField 
-      id="username"
-      label="Username"
-      type="text"
-      name="username"
-      value={username}
-      required
-      onChange={(event) => setUsername(event.target.value)}
-      fullWidth
-    />
-    <TextField 
-      id="password"
-      label="Password"
-      type="password"
-      name="password"
-      value={password}
-      required
-      onChange={(event) => setPassword(event.target.value)}
-      fullWidth
-    />
-    <TextField 
-      id="email"
-      label="Email"
-      type="text"
-      name="email"
-      value={email}
-      required
-      onChange={(event) => setEmail(event.target.value)}
-      fullWidth
-    />
-      <div>
-        {!username && !password && !email ? Hidden(<Button variant="contained" type="submit" name="submit" value="Register">Register</Button>) :
-        <Button variant="contained" type="submit" name="submit" value="Register">Register</Button>}
-      </div>
-  </Stack>
-</form>
-</>
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={20} square>
+      <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}
+      >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">Register User</Typography>
+        {errors.registrationMessage && (
+          <h3 className="alert" role="alert">
+            {errors.registrationMessage}
+          </h3>
+        )}
+      <Box component="form" noValidate onSubmit={registerUser} sx={{ mt: 1 }}>
+        <TextField 
+          id="username"
+          label="Username"
+          type="text"
+          name="username"
+          margin="normal"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required
+          fullWidth
+        />
+        <TextField 
+          id="password"
+          label="Password"
+          type="password"
+          name="password"
+          margin="normal"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+          fullWidth
+        />
+        <TextField 
+          id="email"
+          label="Email"
+          type="text"
+          name="email"
+          margin="normal"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+          fullWidth
+        />
+        <Button
+          value="Register"
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Register
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Link to="/login" variant="body2">
+              {"Already have an account? Login"}
+            </Link>
+          </Grid>
+        </Grid>
+        <Copyright sx={{ mt: 5 }} />
+        </Box>
+      </Box>
+    </Grid>
   );
 } // END OF RegisterForm
 
 export default RegisterForm;
+
+
+
+
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,26 +1,12 @@
-import { Button } from '@mui/material';
 import React from 'react';
 
-import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function RegisterPage() {
-  const history = useHistory();
 
   return (
     <div>
       <RegisterForm />
-
-      <center>
-        <Button
-          variant="text"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </Button>
-      </center>
     </div>
   );
 }
